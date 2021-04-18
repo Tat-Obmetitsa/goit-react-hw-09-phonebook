9593c170f47d8cfa1210dcdc1d003246fbd25231
Tat-Obmetitsa <twinkle.lif.love@gmail.com>
2021-04-19 02:13:56 +0300

    refactor Login\Registration with hooks

--- src/views/LoginView/LoginView.js
+++ src/views/LoginView/LoginView.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { authOperations } from '../../redux/auth';
 import s from './LoginView.module.css';
 
-class LoginView extends Component {
-  state = {
-    email: '',
-    password: '',
-  };
+export default function LoginView() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
+  const dispatch = useDispatch();
+  
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+       case 'email':
+         return setEmail(value);
+       case 'password':
+         return setPassword(value);
+       default:  console.warn(`Option ${name} is not processed!`);
+         return;
+     }
+   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onLogin(this.state);
-
-    this.setState({ name: '', email: '', password: '' });
+    dispatch(authOperations.logIn({email, password}));
+    setEmail('');
+    setPassword('');
   };
-
-  render() {
-    const { email, password } = this.state;
-
     return (
       <div>
         <h1 className={s.form__title}>Authorization</h1>
         <form
-          onSubmit={this.handleSubmit}
+          onSubmit={handleSubmit}
           autoComplete="off"
           className={s.form}
         >
@@ -39,7 +41,7 @@ class LoginView extends Component {
               name="email"
               pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
               value={email}
-              onChange={this.handleChange}
+              onChange={handleChange}
               className={s.form__input}
             />
           </label>
@@ -50,7 +52,7 @@ class LoginView extends Component {
               type="password"
               name="password"
               value={password}
-              onChange={this.handleChange}
+              onChange={handleChange}
               className={s.form__input}
             />
           </label>
@@ -59,11 +61,78 @@ class LoginView extends Component {
         </form>
       </div>
     );
-  }
 }
 
-const mapDispatchToProps = {
-  onLogin: authOperations.logIn,
-};
 
-export default connect(null, mapDispatchToProps)(LoginView);
+
+// Without hooks
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { authOperations } from '../../redux/auth';
+// import s from './LoginView.module.css';
+
+// class LoginView extends Component {
+//   state = {
+//     email: '',
+//     password: '',
+//   };
+
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onLogin(this.state);
+
+//     this.setState({ name: '', email: '', password: '' });
+//   };
+
+//   render() {
+//     const { email, password } = this.state;
+
+//     return (
+//       <div>
+//         <h1 className={s.form__title}>Authorization</h1>
+//         <form
+//           onSubmit={this.handleSubmit}
+//           autoComplete="off"
+//           className={s.form}
+//         >
+//           <label className={s.form__label}>
+//             Email
+//             <input
+//               type="email"
+//               name="email"
+//               pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
+//               value={email}
+//               onChange={this.handleChange}
+//               className={s.form__input}
+//             />
+//           </label>
+
+//           <label className={s.form__label}>
+//             Password
+//             <input
+//               type="password"
+//               name="password"
+//               value={password}
+//               onChange={this.handleChange}
+//               className={s.form__input}
+//             />
+//           </label>
+
+//           <button type="submit" className={s.form__button}>Log in</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   onLogin: authOperations.logIn,
+// };
+
+// export default connect(null, mapDispatchToProps)(LoginView);
--- src/views/RegisterView/RegisterView.js
+++ src/views/RegisterView/RegisterView.js
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { authOperations } from '../../redux/auth';
 import s from './RegisterView.module.css';
 
 
 
-class RegisterView extends Component {
-  state = {
-    name: '',
-    email: '',
-    password: '',
+export default function RegisterView () {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+  
+
+
+  const handleChange = ({ target: { name, value } }) =>  {
+    switch (name) {
+      case 'name':
+        return setName(value);
+      case 'email':
+        return setEmail(value);
+      case 'password':
+        return setPassword(value);
+      default:  console.warn(`Option ${name} is not processed!`);
+        return;
+    }
   };
 
-  handleChange = ({ target: { name, value } }) => {
-    this.setState({ [name]: value });
-  };
-
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-
-    this.props.onRegister(this.state);
-
-    this.setState({ name: '', email: '', password: '' });
+    dispatch(authOperations.register({ name, email, password }))
+    setName('');
+    setEmail('');
+    setPassword('');
   };
-
-  render() {
-    const { name, email, password } = this.state;
-
     return (
       <div >
         <h1 className={s.form__title}>Registration</h1>
 
         <form
-          onSubmit={this.handleSubmit}
+          onSubmit={handleSubmit}
           autoComplete="off"
           className={s.form}
         >
@@ -42,7 +49,7 @@ class RegisterView extends Component {
               type="text"
               name="name"
               value={name}
-              onChange={this.handleChange}
+              onChange={handleChange}
               className={s.form__input}
             />
           </label>
@@ -54,7 +61,7 @@ class RegisterView extends Component {
               name="email"
               pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
               value={email}
-              onChange={this.handleChange}
+              onChange={handleChange}
               className={s.form__input}
             />
           </label>
@@ -65,7 +72,7 @@ class RegisterView extends Component {
               type="password"
               name="password"
               value={password}
-              onChange={this.handleChange}
+              onChange={handleChange}
               className={s.form__input}
             />
           </label>
@@ -74,11 +81,92 @@ class RegisterView extends Component {
         </form>
       </div>
     );
-  }
 }
 
-const mapDispatchToProps = {
-  onRegister: authOperations.register,
-};
 
-export default connect(null, mapDispatchToProps )(RegisterView);
+// Without hooks
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { authOperations } from '../../redux/auth';
+// import s from './RegisterView.module.css';
+
+
+
+// class RegisterView extends Component {
+//   state = {
+//     name: '',
+//     email: '',
+//     password: '',
+//   };
+
+//   handleChange = ({ target: { name, value } }) => {
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+
+//     this.props.onRegister(this.state);
+
+//     this.setState({ name: '', email: '', password: '' });
+//   };
+
+//   render() {
+//     const { name, email, password } = this.state;
+
+//     return (
+//       <div >
+//         <h1 className={s.form__title}>Registration</h1>
+
+//         <form
+//           onSubmit={this.handleSubmit}
+//           autoComplete="off"
+//           className={s.form}
+//         >
+//           <label className={s.form__label}>
+//             Name
+//             <input
+//               type="text"
+//               name="name"
+//               value={name}
+//               onChange={this.handleChange}
+//               className={s.form__input}
+//             />
+//           </label>
+
+//           <label className={s.form__label} >
+//             Email
+//             <input
+//               type="email"
+//               name="email"
+//               pattern="[^@]+@[^@]+\.[a-zA-Z]{2,6}"
+//               value={email}
+//               onChange={this.handleChange}
+//               className={s.form__input}
+//             />
+//           </label>
+
+//           <label className={s.form__label}>
+//             Password
+//             <input
+//               type="password"
+//               name="password"
+//               value={password}
+//               onChange={this.handleChange}
+//               className={s.form__input}
+//             />
+//           </label>
+
+//           <button type="submit" className={s.form__button}>Sign in</button>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
+
+// const mapDispatchToProps = {
+//   onRegister: authOperations.register,
+// };
+
+// export default connect(null, mapDispatchToProps )(RegisterView);
